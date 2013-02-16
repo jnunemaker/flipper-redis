ddca0023ada70c67b38d261a9b178ddc0a1037b1
John Nunemaker <nunemaker@gmail.com>
2013-02-16 10:45:28 -0500

    Docs and updates for latest flipper.

--- lib/flipper/adapters/redis.rb
+++ lib/flipper/adapters/redis.rb
@@ -5,8 +5,12 @@ require 'flipper'
 module Flipper
   module Adapters
     class Redis
+      include Flipper::Adapter
+
+      # Private: The key that stores the set of known features.
       FeaturesKey = :flipper_features
 
+      # Public: The name of the adapter.
       attr_reader :name
 
       def initialize(client)
@@ -14,54 +18,65 @@ module Flipper
         @name = :redis
       end
 
-      # Public
+      # Public: Gets the values for all gates for a given feature.
+      #
+      # Returns a Hash of Flipper::Gate#key => value.
       def get(feature)
         result = {}
-        doc = @client.hgetall(feature.key)
+        doc = doc_for(feature)
+        fields = doc.keys
 
         feature.gates.each do |gate|
           result[gate.key] = case gate.data_type
           when :boolean, :integer
             doc[gate.key.to_s]
           when :set
-            regex = /^#{Regexp.escape(gate.key)}\//
-
-            gate_keys = doc.keys.select { |key| key =~ regex }
-            gate_values = gate_keys.map { |key| key.split('/', 2).last }
-            gate_values.to_set
+            fields_to_gate_value fields, gate
           else
-            unsupported_data_type(gate.data_type)
+            unsupported_data_type gate.data_type
           end
         end
 
         result
       end
 
-      # Public
+      # Public: Enables a gate for a given thing.
+      #
+      # feature - The Flipper::Feature for the gate.
+      # gate - The Flipper::Gate to disable.
+      # thing - The Flipper::Type being disabled for the gate.
+      #
+      # Returns true.
       def enable(feature, gate, thing)
         case gate.data_type
         when :boolean, :integer
           @client.hset feature.key, gate.key, thing.value.to_s
         when :set
-          @client.hset feature.key, "#{gate.key}/#{thing.value}", 1
+          @client.hset feature.key, to_field(gate, thing), 1
         else
-          unsupported_data_type(gate.data_type)
+          unsupported_data_type gate.data_type
         end
 
         true
       end
 
-      # Public
+      # Public: Disables a gate for a given thing.
+      #
+      # feature - The Flipper::Feature for the gate.
+      # gate - The Flipper::Gate to disable.
+      # thing - The Flipper::Type being disabled for the gate.
+      #
+      # Returns true.
       def disable(feature, gate, thing)
         case gate.data_type
         when :boolean
-          @client.del(feature.key)
+          @client.del feature.key
         when :integer
           @client.hset feature.key, gate.key, thing.value.to_s
         when :set
-          @client.hdel feature.key, "#{gate.key}/#{thing.value}"
+          @client.hdel feature.key, to_field(gate, thing)
         else
-          unsupported_data_type(gate.data_type)
+          unsupported_data_type gate.data_type
         end
 
         true
@@ -69,13 +84,35 @@ module Flipper
 
       # Public: Adds a feature to the set of known features.
       def add(feature)
-        @client.sadd(FeaturesKey.to_s, feature.name.to_s)
+        @client.sadd FeaturesKey, feature.name
         true
       end
 
       # Public: The set of known features.
       def features
-        @client.smembers(FeaturesKey.to_s).to_set
+        @client.smembers(FeaturesKey).to_set
+      end
+
+      # Private: Gets a hash of fields => values for the given feature.
+      #
+      # Returns a Hash of fields => values.
+      def doc_for(feature)
+        @client.hgetall(feature.key)
+      end
+
+      # Private: Converts gate and thing to hash key.
+      def to_field(gate, thing)
+        "#{gate.key}/#{thing.value}"
+      end
+
+      # Private: Returns a set of values given an array of fields and a gate.
+      #
+      # Returns a Set of the values enabled for the gate.
+      def fields_to_gate_value(fields, gate)
+        regex = /^#{Regexp.escape(gate.key)}\//
+        keys = fields.grep(regex)
+        values = keys.map { |key| key.split('/', 2).last }
+        values.to_set
       end
 
       # Private
