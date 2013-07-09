fb448af8305fba24222c808977226869baef0805
John Nunemaker <nunemaker@gmail.com>
2013-07-09 16:49:24 -0400

    Remove a bunch of cruft from spec helper.

--- spec/helper.rb
+++ spec/helper.rb
@@ -1,21 +1,6 @@
-$:.unshift(File.expand_path('../../lib', __FILE__))
-
-require 'pathname'
-require 'logger'
-
-root_path = Pathname(__FILE__).dirname.join('..').expand_path
-lib_path  = root_path.join('lib')
-log_path  = root_path.join('log')
-log_path.mkpath
-
 require 'rubygems'
 require 'bundler'
-
-Bundler.require(:default, :test)
-
 require 'flipper-redis'
-
-Logger.new(log_path.join('test.log'))
 
 RSpec.configure do |config|
   config.filter_run :focused => true
